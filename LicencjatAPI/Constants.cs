40dc9fd7461a7401828d58f0aec330165efb3c27
Wurf <havky1@gmail.com>
2021-03-27 16:27:49 +0100

    Dodaj pliki projektów.

---
+++ LicencjatAPI/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicencjatAPI
+{
+    public class Constants
+    {
+        public static string connectionString = "server=185.243.53.232;port=3306;uid=wurf;pwd=GoxFxCRul8N0p2dl;database=wurf";
+
+        #region MYSQL INSERT NEW USER QUERY
+
+        public static string InsertNewUserQuery = $"insert into users (username, password_salt, password, email, created_at) values (@username, @password_salt, @password, @email, @timestamp)";
+
+        #endregion
+
+        #region MYSQL CHECK IF EMAIL ALREADY REGISTERED
+
+        public static string checkIfAlreadyRegisteredQuery = $"select * from users WHERE email LIKE @email";
+
+        #endregion
+    }
+}
---
+++ LicencjatAPI/Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LicencjatAPI.Controllers
+{
+    public class User
+    {
+        public string fullName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        
+        DateTime dtNow = DateTime.Now;
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/register
+        [HttpPost("test")]
+        public IActionResult Register([FromBody]User user)
+            {
+                MySqlConnection conn = new MySqlConnection(Constants.connectionString);
+                MySqlDataReader reader;
+                MySqlCommand checkEmailCommand = new MySqlCommand(Constants.checkIfAlreadyRegisteredQuery, conn);
+                checkEmailCommand.Parameters.Add("@email", MySqlDbType.VarChar).Value = user.email;
+                conn.Open();
+                reader = checkEmailCommand.ExecuteReader();
+                reader.Read();
+                if (reader.HasRows)
+                {
+                    return Unauthorized();
+                }
+                else
+                {
+                    reader.Close();
+                }
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+
+
+                cmd.CommandText = Constants.InsertNewUserQuery;
+                cmd.Parameters.Add("@username", MySqlDbType.VarChar).Value = user.fullName;
+                cmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = user.password;
+                cmd.Parameters.Add("@password_salt", MySqlDbType.VarChar).Value = user.password;
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = user.email;
+                cmd.Parameters.Add("@timestamp", MySqlDbType.DateTime).Value = dtNow;
+                cmd.ExecuteNonQueryAsync();
+
+                conn.Close();
+                    return Created(string.Empty, null);
+
+            
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
